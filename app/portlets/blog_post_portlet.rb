df95e295299d13b9531e65350fcdee3819bcbc42
Jon Leighton <j@jonathanleighton.com>
2010-08-11 08:43:38 +0100

    Fix timezone issue with finding blog post by date

--- app/portlets/blog_post_portlet.rb
+++ app/portlets/blog_post_portlet.rb
@@ -6,7 +6,7 @@ class BlogPostPortlet < Portlet
       @blog_post = scope.find(params[:blog_post_id])
     elsif params[:slug]
       if params[:year]
-        date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+        date = Time.zone.local(params[:year].to_i, params[:month].to_i, params[:day].to_i)
         scope = scope.published_between(date, date + 1.day)
       end
       @blog_post = scope.find_by_slug!(params[:slug])
